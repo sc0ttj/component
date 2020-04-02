2d3882818888c587de20c71ced527029d54871bb
sc0ttj <sc0ttj@users.noreply.github.com>
2020-04-02 23:03:28 +0100

    initial

---
+++ examples/usage-in-node.js
@@ -0,0 +1,137 @@
+var Component = require("../src/component.js")
+
+// ------------------------------------------------
+// USAGE: Defining components
+// ------------------------------------------------
+
+// Define our app state
+var state = {
+  count: 0,
+  incrementBy: 5,
+  id: "foo-id",
+  items: [{ name: "Item one" }, { name: "Item two" }],
+  btnColor: "green"
+}
+
+// Define some generic, re-usable, stateless "sub-components"
+var Heading = text => `<h1>${text}</h1>`
+
+var List = items =>
+  `<ul>${items.map(item => `<li>${item.name}</li>`).join("")}</ul>`
+
+var Button = (label, fn) =>
+  `<button class="btn" onclick=${fn}>${label}</button>`
+
+// Define a stateful main component
+var App = new Component(state)
+
+// Define a view - a function which receives the state as `props`
+// and can returns the view as a string, object, etc
+
+// create an HTML view, using template literals
+var htmlView = props => `
+    <div id=${props.id}>
+      ${Heading("Total so far = " + props.count)}
+      ${List(props.items)}
+      ${Button("Click here", `App.clickBtn(${props.incrementBy})`)}
+    </div>`
+
+// or a JS object view
+var jsonView = props => {
+  return { count: props.count, items: props.items }
+}
+
+// or return the state itself (pure headless component)
+var dataOnlyView = props => props
+
+// Lets use data only for now.. change this if you like
+App.view = dataOnlyView
+
+// Define some events (optional)
+App.clickBtn = props => App.setState({ count: App.state.count + props })
+
+// ------------------------------------------------
+// OPTIONAL: Define chainable "actions", to update the state more easily
+// ------------------------------------------------
+
+// These "actions" are like regular methods, except
+// they're always chainable and tagged by name in
+// your components state history
+App.actions({
+  update: props => App.setState({ props }), // same as calling App.setState()
+
+  reset: props => App.setState({ count: 0 }),
+
+  count: props => App.setState({ count: props }),
+
+  plus: props => App.setState({ count: App.state.count + props }),
+
+  minus: props => App.setState({ count: App.state.count - props }),
+
+  items: props => App.setState({ items: props }),
+
+  addItems: props => App.setState({ items: [...App.state.items, ...props] })
+})
+
+//
+//
+// ------------------------------------------------
+// USAGE: Using the component
+// ------------------------------------------------
+
+//
+// You'd normally use your components inside a web server, etc,
+// in order to do "server side rendering" (SSR)
+//
+
+// Using setState() to trigger a full re-render
+App.setState({
+  items: [{ name: "First" }]
+})
+
+// ...Or call any methods you defined directly
+App.clickBtn(1)
+
+// If you defined some "actions", you can use them
+// to update specific parts of your state
+App.plus(105)
+App.minus(5)
+
+// A components "actions" can be chained
+App.minus(1)
+  .minus(1)
+  .minus(1)
+  .plus(3)
+  .addItems([{ name: "two" }, { name: "three" }])
+
+//
+//
+//
+// Now return the component...
+
+// Returns the current view (and any App styles you
+// defined) as a string, or a JS object
+//
+// (for demo purposes we console log it, so you can see
+// the component rendered to the terminal)
+console.log(App.render())
+
+//
+// ------------------------------------------------
+// OPTIONAL: Using the state "timeline"
+// ------------------------------------------------
+
+// Take a "snapshot" (we'll use it later)
+var snapshot = App.state
+
+App.rewind() // go to initial state
+App.forward() // go to latest state
+App.rewind(2) // rewind two steps to a previous state
+App.forward(2) // fast-forward two steps to a more current state
+App.undo() // same as App.rewind(1)
+App.redo() // same as App.forward(1)
+
+// Set a previous state
+App.setState(App.history[2].state)
+// Set a "named" state, from a previous point in time
+App.setState(snapshot)
---
+++ src/component.js
@@ -0,0 +1,250 @@
+// Begin example implementation
+
+// Note:   This is just a demo/experiment
+// Goals:  Super easy to setup, small code base, automatic re-renders on state update
+//
+// Component() features:
+//  * a state managent thing
+//  * a state history
+//  * automatic re-rendering on state change (no diffing!)
+//  * ability to "time travel" to prev/next states
+//  * update states using simple "actions"
+
+/**
+ * A stateful component creator
+ * @constructor
+ * @param {object} state - The component state (model).
+ */
+function Component(state) {
+  this.reactive = true // if true, re-render on every state change
+  this.log = false //  if true, print state history to console on re-render
+
+  this.isNode =
+    typeof process !== "undefined" &&
+    process !== null &&
+    process.versions !== null &&
+    process.versions.node !== null
+
+  this.state = state // the main state/model of the component
+
+  if (!this.isNode) {
+    // the <style> elem into which we put our component CSS
+    this.styleEl = document.createElement("style")
+    document.head.appendChild(this.styleEl)
+  }
+
+  // on init, add our initial state to the state history
+  this.history = [{ id: 0, state: state, action: "init" }]
+
+  /**
+   * Define chainable named "actions" that will update our
+   * state in specific ways and register in our state history
+   */
+  this.actions = actions => {
+    // convert each key/value to set state function:
+    Object.keys(actions).forEach(action => {
+      if (typeof this[action] !== "undefined") {
+        console.warn("Warning! Not replacing " + this[action])
+        return false
+      }
+      this[action] = newState => {
+        this.action = action
+        return actions[action](newState)
+      }
+    })
+
+    return this
+  }
+
+  this.deepFreeze = obj => {
+    if (!Object.isFrozen(obj)) {
+      // Recusively call until all child objects are frozen
+      Object.keys(obj).forEach(name => this.deepFreeze(obj[name]))
+      Object.freeze(obj)
+    }
+    return obj
+  }
+
+  /**
+   * Set the component state
+   * @param {object} newState - the new state to update to
+   */
+  this.setState = newState => {
+    // update previous and current state
+    this.previousState = this.state
+    this.state = { ...this.state, ...newState }
+    // re-render component
+    if (this.reactive) this.render(this.container)
+    // update history and move along index
+    if (this.currentIndex === this.history.length) {
+      // we are not traversing state history, so add the new state to history
+      this.history.push({
+        id: this.history.length,
+        state: this.previousState,
+        action: this.action || "setState"
+      })
+    }
+    this.currentIndex = this.history.length
+    // log state changes
+    if (this.log) console.log(this.currentIndex, [this.state, ...this.history])
+
+    this.action = undefined
+
+    // freeze state so it can only be changed through setState()
+    this.deepFreeze(this.state)
+
+    return this
+  }
+
+  /**
+   * Travel forward/backward to different states in the state history
+   * @param {number} num - the number of steps through the history to take
+   * @param {string} direction - 'f' for forward, 'b' for backwards
+   */
+  this.travel = function(num, direction) {
+    var newIndex
+    if (direction === "f") {
+      newIndex = this.currentIndex + num
+    } else {
+      newIndex = this.currentIndex - num
+    }
+    this.setState(this.history[newIndex].state)
+    this.currentIndex = newIndex
+
+    return this
+  }
+
+  /**
+   * Go to previous (or first) state in the state history
+   * @param {number} num - the number of steps through the history to take, 0 if 'num' not given
+   */
+  this.rewind = function(num) {
+    if (!num) {
+      this.setState(this.history[0].state)
+      this.currentIndex = 0
+      return true
+    }
+    this.travel(num, "b")
+
+    return this
+  }
+
+  /**
+   * Go to more recent state in the state history
+   * @param {number} num - the number of steps through the history to take, most recent if 'num' not given
+   */
+  this.forward = function(num) {
+    if (!num) {
+      this.setState(this.history[this.history.length - 1].state)
+      this.currentIndex = this.history.length - 1
+      return true
+    }
+    this.travel(num, "f")
+
+    return this
+  }
+
+  /**
+   * Go go back one step in the state history
+   */
+  this.undo = function() {
+    this.rewind(1)
+
+    return this
+  }
+
+  /**
+   * Go go forward one step in the state history
+   */
+  this.redo = function() {
+    this.forward(1)
+
+    return this
+  }
+
+  /**
+   * If in browser, puts our component styles in this.styleEl
+   */
+  this.setStyles = function() {
+    if (typeof document !== "undefined" && document.querySelector) {
+      // add our styles
+      if (this.styleEl) {
+        this.styleEl.innerHTML = this.style(this.state) || ""
+      }
+    }
+  }
+
+  /*
+  * Render the current view as a string, including
+  * our component styling, in a <style> tag
+  */
+  this.renderToString = function() {
+    var str = ""
+    var view = this.view(this.state)
+    var style
+
+    if (typeof this.style === "function") {
+      style = this.style(this.state)
+    }
+
+    if (typeof view === "string") {
+      try {
+        // if view is a JSON string
+        str = JSON.parse(view)
+        // return the view as prettified JSON
+        str = JSON.stringify(str, null, 2)
+      } catch (err) {
+        if (style) str = `<style>${style}\n</style>\n`
+        str += `${view}`
+      }
+    } else if (typeof view === "object" || typeof view === "array") {
+      // return the view as prettified JSON
+      str = JSON.stringify(view, null, 2)
+    }
+    //if (this.log) console.log(str)
+    return str
+  }
+
+  /**
+   * Re-render the component and add it to the page
+   * @param {string|element} container - the element which holds the component
+   */
+  this.render = function(container) {
+    var view = this.view(this.state)
+    if (this.isNode) {
+      return this.renderToString()
+    } else {
+      var el = container
+      if (this.styleEl) this.setStyles()
+      if (typeof el === "string") el = document.querySelector(el)
+      this.container = el
+      //
+      // For diffing of real DOM nodes:
+      //
+      // here is where you would diff this.view this el.innerHTML
+      // and apply only the differences.. see nanohtml and nanomorph
+      //
+      // For diffing of VDOM:
+      //
+      // here you would generate VDOM from the `this.view` template string
+      // (see snabby, nano-byte, vel, etc), then do VDOM diffing (see main-loop, etc),
+      // then do VDOM to real DOM (nano-byte, jsx-pragmatic, etc)
+      //
+      // For better performance, through batched rendering, we can use
+      // requestAnimationFrame (see Raynos/main-loop)
+      //
+      // ... we have no diffing setup, lets just replace the whole thing :/
+      el.innerHTML = view
+    }
+    return this.container
+  }
+
+  return this
+}
+
+if (typeof module === "undefined") {
+  module = {}
+  module.exports = {}
+}
+
+module.exports = Component
